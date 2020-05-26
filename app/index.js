8cfecbe8f1cd3ae2cbe81df848ef76d43d6f481c
Staszek <beben86@wp.pl>
2020-05-26 18:06:26 +0200

    Responive WiP

--- app/index.js
+++ app/index.js
@@ -27,3 +27,38 @@ window.addEventListener('scroll', function(){
     }
     prevScrollpos = currentScrollpos;
 })
+
+
+const closeButton = document.querySelector('.close-btn');
+const closeEmailButton = document.querySelector('.close-email');
+const cvContainer = document.querySelector('.overlay');
+const cvButton = document.querySelector('.cv');
+const emailButton = document.querySelector('.email-icon');
+const emailBox = document.querySelector('.email-address');
+
+
+closeButton.addEventListener("click", function(){
+  // console.log("click" );
+  cvContainer.style.display = "none"
+  
+})
+
+cvButton.addEventListener("click", function(){
+  // console.log("click" );
+  cvContainer.style.display = "flex"
+  
+  
+});
+
+closeEmailButton.addEventListener("click", function(){
+  emailBox.classList.add('hide')
+  // console.log('hide')
+  
+})
+
+
+
+emailButton.addEventListener("click", function(){
+  // console.log('amail icon click');
+  emailBox.classList.remove('hide');
+})
